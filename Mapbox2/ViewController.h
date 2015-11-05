ea97f14433cc3491fef58b21ddce73b54c5c6385
Stephen Harle <stephenharle2015@u.northwestern.edu>
2015-11-04 22:01:52 -0600

    Initial Commit

---
+++ Mapbox2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Mapbox2
+//
+//  Created by Molly on 11/4/15.
+//  Copyright © 2015 Molly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Mapbox2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Mapbox2
+//
+//  Created by Molly on 11/4/15.
+//  Copyright © 2015 Molly. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
